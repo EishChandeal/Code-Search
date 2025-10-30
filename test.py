64eacda20c533d3432acd432383b53edc84234ab
Eish Chandeal <eishchandeal25@gmail.com>
2025-10-30 14:52:03 +0530

    Found codebert-base embedding model online tried using it, took a while and then generated a matrix encoding it seems

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 from sentence_transformers import SentenceTransformer
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
-print("so does poetry work now?")
+pipe = pipeline("feature-extraction", model="microsoft/codebert-base")
+# from transformers import AutoTokenizer, AutoModel
+# import torch
+# tokenizer = AutoTokenizer.from_pretrained("microsoft/codebert-base")
+# model = AutoModel.from_pretrained("microsoft/codebert-base")
+
+print(pipe("def max(a,b): if a>b: return a else return b"))
+
+print("Mic testing..")
